0907a1182bbd449b0fa26fa815f6c889d80a8bab
xianzhixianzhixian <MrRobotFan@163.com>
2019-08-08 23:13:59 +0800

    eureka消费者，通过Ribbon去调用，默认是负载均衡的

---
+++ eureka-consumer/src/main/java/com/eureka/eurekaconsumer/EurekaConsumerApplication.java
@@ -0,0 +1,23 @@
+package com.eureka.eurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
