f7151bcf7ee4a57a6d25a141f2ffd9414c84805b
xianzhixianzhixian <MrRobotFan@163.com>
2019-08-23 00:40:38 +0800

    结合Hystrix

--- consul-feign/src/main/java/com/feign/controller/ConsulFeignController.java
+++ consul-feign/src/main/java/com/feign/controller/ConsulFeignController.java
@@ -1,6 +1,7 @@
 package com.feign.controller;
 
 import com.feign.util.MemberFeign;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,13 @@ public class ConsulFeignController {
     @Autowired
     private MemberFeign memberFeign;
 
+    @HystrixCommand(fallbackMethod = "fallback")
     @RequestMapping(value = "/getServices")
     public String getServices() {
         return memberFeign.getServices();
+    }
+
+    private String fallback() {
+        return "服务器访问人数过多，请稍后再试";
     }
 }
